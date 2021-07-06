a90c12fdaeab46ffc2b1622a7e64cf553a20dec2
marian.olar <marian.olar@mtxb2b.com>
2021-07-06 12:04:08 +0300

    Add cors config

---
+++ src/main/java/ro/fortech/comments/config/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package ro.fortech.comments.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry
+                .addMapping("/**")
+                .allowedMethods("GET", "PUT", "POST", "DELETE");
+    }
+}
